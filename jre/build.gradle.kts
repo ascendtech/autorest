550e12c023810356d14422f6de80547cb1ef1128
Matthew Davis <matt.davis@ascend-tech.us>
2020-11-25 16:36:55 -0500

    update gson

--- jre/build.gradle.kts
+++ jre/build.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
     implementation("javax.ws.rs:jsr311-api:1.1.1")
     implementation("javax.inject:javax.inject:1")
     implementation("io.reactivex.rxjava2:rxjava:2.2.10")
-    implementation("com.google.code.gson:gson:2.8.2")
+    implementation("com.google.code.gson:gson:2.8.6")
     testAnnotationProcessor(project(":autorest-processor"))
     testImplementation("junit:junit:4.12")
 }
