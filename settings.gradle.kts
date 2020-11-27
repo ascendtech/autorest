b45cf177e6fb432302a0efeb61727861f1d46bb5
Matthew Davis <matt.davis@ascend-tech.us>
2020-11-27 13:00:31 -0500

    rename modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,8 +2,4 @@ rootProject.name = "autorest-parent"
 include(":autorest-processor")
 include(":autorest-core")
 include(":autorest-gwt")
-include(":autorest-jre")
-project(":autorest-processor").projectDir = file("processor")
-project(":autorest-core").projectDir = file("core")
-project(":autorest-gwt").projectDir = file("gwt")
-project(":autorest-jre").projectDir = file("jre")
+
