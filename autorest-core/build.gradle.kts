dd53022ee8473ee5a63afcc90e919e493f7bb2bb
Matthew Davis <matt.davis@ascend-tech.us>
2020-11-27 13:01:01 -0500

    bump junit

--- autorest-core/build.gradle.kts
+++ autorest-core/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-    testImplementation("junit:junit:4.12")
+    testImplementation("junit:junit:4.13.1")
     compileOnly("javax.ws.rs:jsr311-api:1.1.1")
     compileOnly("javax.inject:javax.inject:1")
 }
